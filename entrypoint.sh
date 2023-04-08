e81d1d66c87a9cd53ec16157f9252aa7a191b845
lizheming <i@imnerd.org>
2023-04-08 20:29:25 +0800

    chore: add env export

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,42 +6,42 @@ export GITHUB="true"
 
 if [ -n "$INPUT_ACCESS_TOKEN" ]
 then
-  SPACE_ACCESS_TOKEN="$INPUT_ACCESS_TOKEN"
+  export SPACE_ACCESS_TOKEN="$INPUT_ACCESS_TOKEN"
 fi
 
 if [ -n "$INPUT_ID" ]
 then
-  SPACE_ID="$INPUT_ID"
+  export SPACE_ID="$INPUT_ID"
 fi
 
 if [ -n "$INPUT_TAG" ]
 then
-  SPACE_PUSH_TAG="$INPUT_TAG"
+  export SPACE_PUSH_TAG="$INPUT_TAG"
 fi
 
 if [ -n "$INPUT_DIR" ]
 then
-  SPACE_DIR="$INPUT_DIR"
+  export SPACE_DIR="$INPUT_DIR"
 fi
 
 if [ -n "$INPUT_LISTED" ]
 then
-  SPACE_LISTED="$INPUT_LISTED"
+  export SPACE_LISTED="$INPUT_LISTED"
 fi
 
 if [ -n "$INPUT_NOTES" ]
 then
-  SPACE_NOTES="$INPUT_NOTES"
+  export SPACE_NOTES="$INPUT_NOTES"
 fi
 
 if [ -n "$INPUT_RID" ]
 then
-  SPACE_RID="$INPUT_RID"
+  export SPACE_RID="$INPUT_RID"
 fi
 
 if [ -n "$INPUT_VERSION" ]
 then
-  SPACE_PUSH_TAG="$INPUT_VERSION"
+  export SPACE_PUSH_TAG="$INPUT_VERSION"
 fi
 
 # debug
